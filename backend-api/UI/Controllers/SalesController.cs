966129ffe1556af6c844802f8ab4080ab37530dd
Caio Ferraz <caio.aguiar@capgemini.com>
2024-12-18 19:36:15 -0300

    doc(api): add doc para endpoint de registro de venda

--- backend-api/UI/Controllers/SalesController.cs
+++ backend-api/UI/Controllers/SalesController.cs
@@ -1,15 +1,20 @@
 using System.Net;
 using AutoMapper;
 using backend_api.Application.DTOs;
+using backend_api.Application.Filters;
 using backend_api.Application.Helpers;
 using backend_api.Domain.Interfaces.Services;
 using backend_api.Domain.Models;
+using backend_api.UI.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend_api.UI.Controllers;
 
 [ApiController]
 [Route("api/sales")]
+[ValidateModel]
+[Consumes("application/json")]
+[Produces("application/json")]
 public class SalesController : ControllerBase
 {
     private readonly ISaleService _saleService;
@@ -21,6 +26,19 @@ public class SalesController : ControllerBase
         _mapper = mapper;
     }
 
+    /// <summary>
+    /// Registra uma nova venda.
+    /// </summary>
+    /// <response code="201">Sucesso ao registrar a venda.</response>
+    /// <response code="400">Dados inválidos.</response>
+    /// <response code="404">Item não encontrado.</response>
+    /// <response code="422">Estoque insuficiente.</response>
+    /// <response code="500">Erro inesperado.</response>
+    [ProducesResponseType(typeof(ApiResponse<BookDto>), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status422UnprocessableEntity)]
+    [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status500InternalServerError)]
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] RegisterSaleDto saleDto)
     {
